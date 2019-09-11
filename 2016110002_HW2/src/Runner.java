d37c6522e39f42eab56995b586f94ae37c11a11e
jam <gpalektzja@gmail.com>
2019-09-11 22:22:19 +0900

    Add draft code

---
+++ 2016110002_HW2/src/Runner.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static int ccw(ArrayList<Integer> a, ArrayList<Integer> b, ArrayList<Integer> c) {
+		// get counterclockwise of point c for point a and b
+	}
+	
+	public static int[] getStartEndPointIdx(ArrayList<Integer> a, ArrayList<Integer> b, ArrayList<Integer> c) {
+		// get start point and end point indices 
+	}
+	
+	public static boolean isIntersect(ArrayList<ArrayList<Integer>> ab, ArrayList<ArrayList<Integer>> cd) {
+		
+	}
+	
+	public static ArrayList<Integer> getIntersection(ArrayList<ArrayList<Integer>> ab, ArrayList<ArrayList<Integer>> cd){
+		
+	}
+	
+	public static float getSlope(ArrayList<ArrayList<Integer>> ab) {
+		
+	}
+	
+	public static float getYIntercept(ArrayList<ArrayList<Integer>> ab) {
+		
+	}
+	
+	public static ArrayList<ArrayList<Integer>> sortIntersection(HashSet<ArrayList<Integer>> uniqueInter){
+		
+	}
+}
