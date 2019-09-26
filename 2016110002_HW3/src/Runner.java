35ab29a5604524d765749f2c126c703cd2e5e01d
jam <gpalektzja@gmail.com>
2019-09-26 23:17:52 +0900

    Add file io and getCoeffi

--- 2016110002_HW3/src/Runner.java
+++ 2016110002_HW3/src/Runner.java
@@ -1,20 +1,50 @@
 import java.lang.Math;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 
 public class Runner {
 
-	public static void main(String[] args) {
-		int max_deg = 7;
-		int n = getN(max_deg);
+	public static void main(String[] args) throws IOException {
+		String inputPath = "/Users/poza/jam/2019_2/algorithm_analysis/input.txt";
+		FileReader fileReader = new FileReader(inputPath);
+		BufferedReader bufferReader = new BufferedReader(fileReader);
 		
-		int[] a = new int[]{0, 1, 2, 3, 4, 5, 6, 7};
-		Complex[] A = new Complex[n];
+		String outputPath = "/Users/poza/jam/2019_2/algorithm_analysis/2016110002.txt";
+		File file = new File(outputPath);
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
 		
-		fft(a, A);
+		int nCase = Integer.parseInt(bufferReader.readLine().trim());
 		
-		for (Complex c: A) {
-			c.roundBySeventhDecimal();
-			System.out.println(c.real+" "+c.imag);
+		for(int i = 0; i < nCase; i++) {
+			int testNum = i + 1;
+			String[] splited = bufferReader.readLine().trim().split(" ");
+			int max_deg = Integer.parseInt(splited[0]);
+			int n = getN(max_deg);
+			
+			int[] a = getCoeffi(n, splited);
+			Complex[] A = new Complex[n];
+			
+			fft(a, A);
+			
+			writeOutput(testNum, A, bufferedWriter);
 		}
+		bufferReader.close();
+		bufferedWriter.close();
+	}
+	
+	
+	public static int[] getCoeffi(int n, String[] splited) {
+		int[] a = new int[n];
+		for(int i = 0; i < n; i++) {
+			if (i + 1 < splited.length) {
+				a[i] = Integer.parseInt(splited[i+1]);
+			}
+		}
+		return a;
 	}
 	
 	public static void fft(int[] a, Complex[] A) {
@@ -50,6 +80,7 @@ public class Runner {
 			A[k + n/2] = Complex.sum(even_A[k], Complex.multi(wk, odd_A[k]));
 		}
 	}
+	
 	public static int getN(int max_deg) {
 		// maximum of max_deg = 63, so max 2^k = 64, k = 6
 		int max_k = 6;
@@ -68,6 +99,16 @@ public class Runner {
 		for(int i = 0; i < a.length / 2; i++) {
 			even[i] = a[i * 2];
 			odd[i] = a[i * 2 + 1];
+		}
+	}
+	
+	public static void writeOutput(int testNum, Complex[] A, BufferedWriter bufferedWriter) throws IOException {
+		bufferedWriter.write("#"+testNum);
+		bufferedWriter.newLine();
+		for (Complex c: A) {
+			c.roundBySeventhDecimal();
+			bufferedWriter.write(String.format("%.6f %.6f", c.real, c.imag));
+			bufferedWriter.newLine();
 		}
 	}
 }
