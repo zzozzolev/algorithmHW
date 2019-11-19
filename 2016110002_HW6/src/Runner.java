eac61abe04a8f89e50d317c68ae1190adf25607d
jam <gpalektzja@gmail.com>
2019-11-20 00:00:11 +0900

    Implment main loop and switch functions location

--- 2016110002_HW6/src/Runner.java
+++ 2016110002_HW6/src/Runner.java
@@ -9,7 +9,38 @@ public class Runner {
 		int[][] constCoeff = new int[m][n];
 		ArrayList<Double> objValue = new ArrayList<Double>();
 		
+		addNonNegativity(constCoeff);
 		
+		boolean isAllNeg = isObjFuncAllNeg(constCoeff[0]);
+		boolean unbounded = false;
+		// 0: constant, 1: x_1, ...
+		int[][] slackForm = getSlackForm(constCoeff);
+		
+		while(!isAllNeg || unbounded) {
+			int maxCoeffIdx = getMaxCoeffIdx(slackForm[0]);
+			if (maxCoeffIdx == 0)
+				System.out.println("constant is maxCoeffIdx");
+			unbounded = isUnbounded(maxCoeffIdx, slackForm);
+			
+			if (unbounded)
+				break;
+			
+			// (row, col) (const, coeff)
+			int tightConstIdx = getTightConstIdx(maxCoeffIdx, slackForm);
+			int[] rearranged = getRearranged(maxCoeffIdx, slackForm[tightConstIdx]);
+			
+			replace(rearranged, slackForm);
+			
+			objValue.add(slackForm[0][0]);
+		}
+	}
+	
+	public static void addNonNegativity(int[][] constCoeff) {
+	 
+	}
+	
+	public static boolean isObjFuncAllNeg(int[] objFunc) {
+		// TODO: do not consider constant
 	}
 	
 	public static int[][] getSlackForm(int[][] constCoeff) {
@@ -17,25 +48,24 @@ public class Runner {
 		// [m+n][n+1]
 	}
 	
-	public static void addNonNegativity(int[][] constCoeff) {
-		
-	}
-	
 	public static int getMaxCoeffIdx(int[] objFuncCoeff) {
+		// TODO: do not consider constant
+	}
+	
+	public static boolean isUnbounded(int maxCoeffIdx, int[][] slackForm) {
 		
 	}
 	
-	public static boolean isUnbounded(int maxCoeffIdx, int[][] constCoeff) {
+	public static int getTightConstIdx(int maxCoeffIdx, int[][] slackForm) {
 		
 	}
 	
-	public static int getTightConstIdx(int[][] slackForm) {
-		
+	public static int[] getRearranged(int maxCoeffIdx, int[] constraint) {
+		// TODO: max coeff == 0
 	}
 	
-	public static int[] getRearranged(int[][] slackForm) {
+	public static void replace(int[] rearranged, int[][] slackForm) {
 		
 	}
-	
 	
 }
